e736d30e57980f22fa3f25912f2197f303149d8f
findridoy <findridoy@gmail.com>
2023-02-20 16:55:17 +0600

    Update log.go

--- log.go
+++ log.go
@@ -6,11 +6,43 @@ import (
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
+type ErrorLogger struct {
+	driver *zap.Logger
+}
+
 type Logger struct {
 	driver *zap.Logger
 }
 
+// NewLogger will
 func NewLogger(dir string) *Logger {
+	w := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   "/var/log/" + dir + "/info.log",
+		MaxSize:    25, // megabytes
+		MaxBackups: 20,
+		MaxAge:     28, // days
+	})
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "time"
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		w,
+		zap.InfoLevel,
+	)
+
+	logger := new(Logger)
+	logger.driver = zap.New(core)
+	return logger
+}
+
+func (infoLogger Logger) Info(msg string) {
+	infoLogger.driver.Info(msg)
+}
+
+func NewErrorLogger(dir string) *ErrorLogger {
 	w := zapcore.AddSync(&lumberjack.Logger{
 		Filename:   "/var/log/" + dir + "/error.log",
 		MaxSize:    25, // megabytes
@@ -28,15 +60,11 @@ func NewLogger(dir string) *Logger {
 		zap.InfoLevel,
 	)
 
-	logger := new(Logger)
+	logger := new(ErrorLogger)
 	logger.driver = zap.New(core, zap.AddCallerSkip(2), zap.AddCaller())
 	return logger
 }
 
-func (logger Logger) Info() {
-
-}
-
-func (logger Logger) Error(msg string) {
+func (logger ErrorLogger) Error(msg string) {
 	logger.driver.Error(msg)
 }
